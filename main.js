cefa5f575a1d6d49a2c2b16b5ae21c9f8c0554ed
Tomas <112868952+TomyReiv@users.noreply.github.com>
2023-03-24 12:54:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,17 +15,31 @@ const resp = (param) => {
 
 //validador
 const val = () => {
-  divisa = prompt("¿Que divisa desea cotizar (Dolares o euros)?").toLowerCase();
+  divisa = prompt("¿Que divisa desea cotizar (Dolares o euros)?").toLowerCase().trim();
 
   if (divisa != "dolar" && divisa != "euro") {
     alert("Solo se podra cotizar dolares o euros");
     return;
   }
 
-  operacion = prompt("¿Que operacion desea hacer?").toLowerCase();
+  operacion = prompt("¿Que operacion desea hacer?").toLowerCase().trim();
 
   if (operacion != "comprar" && operacion != "vender") {
     alert("Solo se podra cotizar compras o ventas de divisas");
+    return;
+  }
+};
+
+//funcion de ingreso de datos
+
+const ingresarValor = (param) => {
+  valor = parseInt(prompt(param));
+};
+
+//validar que sea un numero
+const valNumero = (param) => {
+  if (isNaN(param)) {
+    alert("El valor agregado no puede ser procesado");
     return;
   }
 };
@@ -38,29 +52,29 @@ const convertidor = (operador) => {
     val();
 
     if (divisa == "dolar" && operacion == "vender") {
-      valor = parseInt(
-        prompt("Introduzca el monto de dolares que desae vender")
-      );
-      resultado = valor * dolarCompra;
-      resp("Recibirá $");
+      ingresarValor("Introduzca el monto de dolares que desae vender");
+      if (!valNumero(valor)) {
+        resultado = valor * dolarCompra;
+        resp("Recibirá $");
+      }
     } else if (divisa == "dolar" && operacion == "comprar") {
-      valor = parseInt(
-        prompt("Introduzca el monto de dolares que desae comprar")
-      );
-      resultado = valor * dolarVenta;
-      resp("Deberá pagar $");
+      ingresarValor("Introduzca el monto de dolares que desae comprar");
+      if (!valNumero(valor)) {
+        resultado = valor * dolarVenta;
+        resp("Deberá pagar $");
+      }
     } else if (divisa == "euro" && operacion == "vender") {
-      valor = parseInt(
-        prompt("Introduzca el monto de euros que desae vender")
-      );
-      resultado = valor * euroCompra;
-      resp("Recibirá $");
+      ingresarValor("Introduzca el monto de euros que desae vender");
+      if (!valNumero(valor)) {
+        resultado = valor * euroCompra;
+        resp("Recibirá $");
+      }
     } else if (divisa == "euro" && operacion == "comprar") {
-      valor = parseInt(
-        prompt("Introduzca el monto de euros que desae comprar")
-      );
-      resultado = valor * euroVenta;
-      resp("Deberá pagar $");
+      ingresarValor("Introduzca el monto de euros que desae comprar");
+      if (!valNumero(valor)) {
+        resultado = valor * euroVenta;
+        resp("Deberá pagar $");
+      }
     }
     respuesta = confirm("¿Desea hacer otro calculo?");
   }
