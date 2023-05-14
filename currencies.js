b17b77a49bdf1b693ef913cd7b7d11e7fe129522
Tomas <112868952+TomyReiv@users.noreply.github.com>
2023-05-14 19:10:00 -0300

    Add try catch

--- currencies.js
+++ currencies.js
@@ -1,40 +1,44 @@
-
 const currenciesList = async () => {
-  const resp = await fetch("generated.json");
-  const json = await resp.json();
+  try {
+    const resp = await fetch("generated.json");
+    const json = await resp.json();
+    const desde = document.querySelector("#de");
+    const hasta = document.querySelector("#a");
 
-  const desde = document.querySelector("#de");
-  const hasta = document.querySelector("#a");
-
-  for (const [key, value] of Object.entries(json)) {
-    let optionElement = document.createElement("option");
-    optionElement.value = key;
-    optionElement.text = value;
-    hasta.appendChild(optionElement);
-  }
-  for (const [key, value] of Object.entries(json)) {
-    let optionElement = document.createElement("option");
-    optionElement.value = key;
-    optionElement.text = value;
-    desde.appendChild(optionElement);
-  }
-
-  for (let i = 0; i < desde.options.length; i++) {
-    let option = desde.options[i];
-
-    if (option.value === historial.base) {
-      option.selected = true;
-      break;
+    for (const [key, value] of Object.entries(json)) {
+      let optionElement = document.createElement("option");
+      optionElement.value = key;
+      optionElement.text = value;
+      hasta.appendChild(optionElement);
     }
-  }
-
-  for (let i = 0; i < hasta.options.length; i++) {
-    let option = hasta.options[i];
-
-    if (option.value === historial.currencies) {
-      option.selected = true;
-      break;
+    for (const [key, value] of Object.entries(json)) {
+      let optionElement = document.createElement("option");
+      optionElement.value = key;
+      optionElement.text = value;
+      desde.appendChild(optionElement);
     }
+
+    for (let i = 0; i < desde.options.length; i++) {
+      let option = desde.options[i];
+
+      if (option.value === historial.base) {
+        option.selected = true;
+        break;
+      }
+    }
+
+    for (let i = 0; i < hasta.options.length; i++) {
+      let option = hasta.options[i];
+
+      if (option.value === historial.currencies) {
+        option.selected = true;
+        break;
+      }
+    }
+  } catch (error) {
+    console.error("Error en la función currenciesList:", error);
   }
 };
+
+
 currenciesList();
